05244d68750323e7da690543a0b722e44b91b255
Stephen Brennan <sb185482@ncr.com>
2020-07-29 12:40:14 +0100

    Update deploy commnands

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
-docker build --tag userservice:1.0 .
-docker tag userservice:1.0 gcr.io/datanauts/userservice:1.0
-gcloud auth login
-docker push gcr.io/datanauts/userservice:1.0
+docker build --tag userservice .
+docker tag userservice gcr.io/datanauts/userservice
+docker push gcr.io/datanauts/userservice
 gcloud container clusters get-credentials cluster-1 --zone us-central1-c --project datanauts
 kubectl apply -f ./userservice.yaml
-
-kubectl get services
